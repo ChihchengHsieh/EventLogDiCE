1bd6e3be658145010d6b83e6987089cc801108f1
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-06-29 19:43:10 +1000

    updating result.

--- parameters/model.py
+++ parameters/model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class LSTMPredNextWithResourceModelParameters(object):
     activity_embedding_dim: int = 32
@@ -11,6 +12,15 @@ class LSTMPredNextWithResourceModelParameters(object):
 
 @dataclass
 class LSTMScenarioCfWithResourceModelParameters(object):
+    activity_embedding_dim: int = 32
+    resource_embedding_dim: int = 128
+    lstm_hidden: int = 64
+    dense_dim: int = 64
+    dropout: float = 0.1
+
+
+@dataclass
+class LSTMPredNextAmountSpecificModelParameters(object):
     activity_embedding_dim: int = 32
     resource_embedding_dim: int = 128
     lstm_hidden: int = 64
