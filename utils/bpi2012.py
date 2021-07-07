53bacbe0c0964d7a1cece0a89b1b0bbdeb684cce
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-07-07 16:10:18 +1000

    refactor tail remove code.

--- utils/bpi2012.py
+++ utils/bpi2012.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -74,3 +75,21 @@ def generate_fake_df(size: int, activity_feature_names: List[str], resource_feat
     fake_df['predicted'] = np.random.choice([0, 1], size)
 
     return fake_df
+
+
+def get_example_data_with_removed_tail(dataset, trace_idx, tail_length_to_remove=0):
+    _, example_activities, _, example_resources, example_amount, _ = dataset.collate_fn([trace_idx])
+    print_block(dataset.activity_vocab.list_of_index_to_vocab_2d(example_activities),"Activity")
+    print_block(dataset.resource_vocab.list_of_index_to_vocab_2d(example_resources),"Resource")
+    print_block(example_amount, "Amount")
+
+    example_idx_activities, example_idx_resources = remove_trail_steps(example_activities, example_resources, tail_length_to_remove)
+    example_vocab_activities = dataset.activity_vocab.list_of_index_to_vocab_2d(example_idx_activities)[0]
+    example_vocab_resources = dataset.resource_vocab.list_of_index_to_vocab_2d(example_idx_resources)[0]
+    ground_truth_vocab =dataset.activity_vocab.index_to_vocab(example_activities[:, len(example_vocab_activities)][0])
+
+    print_block(example_vocab_activities, "Activity (Tail removed)")
+    print_block(example_vocab_resources, "Resource (Tail removed)")
+    print_block(ground_truth_vocab, "Ground Truth")
+
+    return example_vocab_activities, example_idx_activities, example_vocab_resources, example_idx_resources, example_amount
